2ba491b6ea3f851da7a2f3e7e3214a47f4723daa
Brenno Gomes <brennogbreda@gmail.com>
2025-10-16 16:57:54 -0300

    Initial Commit

---
+++ Whatodo/WhatodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatodoApp.swift
+//  Whatodo
+//
+//  Created by Brenno Gomes Breda on 16/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WhatodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
